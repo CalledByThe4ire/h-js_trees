0778956b5eea62797119df02aebc75c0dcb4b4e6
Andrey Karamnoff <karamnoff@gmail.com>
2018-09-19 16:34:49 +0300

    add types for 'traversal' exercise-unit

--- src/downcaseFileNames.js
+++ src/downcaseFileNames.js
@@ -1,4 +1,22 @@
 // @flow
 // BEGIN (write your solution here)
+type BaseNode = { name: string, meta: {} };
+type File = BaseNode & {
+  type: 'file'
+};
 
+/* eslint-disable no-use-before-define */
+type Directory = BaseNode & {
+  type: 'directory',
+  children: Array<Node>
+};
+/* eslint-enable no-use-before-define */
+
+type Node = File | Directory;
+
+const downcaseFileNames = (tree: Node): Node => {
+  
+};
+
+export default downcaseFileNames;
 // END
