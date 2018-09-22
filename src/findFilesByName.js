48603eeae0984060addd270a88ee028dcf9c6263
Andrey Karamnoff <karamnoff@gmail.com>
2018-09-22 08:35:18 +0300

    add solution for search

--- src/findFilesByName.js
+++ src/findFilesByName.js
@@ -1,6 +1,40 @@
-// @flow
 import path from 'path';
+// @flow
+type BaseNode = { name: string, meta: {} };
+type File = BaseNode & {
+  type: 'file'
+};
+
+/* eslint-disable no-use-before-define */
+type Directory = BaseNode & {
+  type: 'directory',
+  children: Array<Node>
+};
+/* eslint-enable no-use-before-define */
+
+type Node = File | Directory;
 
 // BEGIN (write your solution here)
+export default (root: Node, substr: string): Array<string> => {
+  const iter = (
+    tree: Node,
+    combination: string,
+    segmentsList: string,
+    acc: Array<string>,
+  ): Array<String> => {
+    if (tree.children) {
+      return tree.children.reduce(
+        (cAcc: Array<string>, child: Array<Node>) =>
+          iter(child, combination, path.join(segmentsList, child.name), cAcc),
+        acc,
+      );
+    }
 
+    if (tree.type === 'file' && tree.name.includes(combination)) {
+      return [...acc, path.join(segmentsList)];
+    }
+    return acc;
+  };
+  return iter(root, substr, root.name, []);
+};
 // END
