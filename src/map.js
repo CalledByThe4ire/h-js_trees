8862d7ded9dce8dd22e53636714da2f20b8e11af
Andrey Karamnoff <karamnoff@gmail.com>
2018-09-23 21:12:31 +0300

    add hexlet' solution for map

--- src/map.js
+++ src/map.js
@@ -1,34 +1,26 @@
 // @flow
-// BEGIN (write your solution here)
-
 type BaseNode = { name: string, meta: {} };
 type File = BaseNode & {
-  type: 'file'
+  type: "file"
 };
 
 /* eslint-disable no-use-before-define */
 type Directory = BaseNode & {
-  type: 'directory',
+  type: "directory",
   children: Array<Node>
 };
 /* eslint-enable no-use-before-define */
 
 type Node = File | Directory;
 
-const map = (func: (item: {name: string}) => Directory, tree: Node): Node => {
-  const { children }: { children: Array<Node> } = (tree: Node);
-  const { name: updatedName }: { name: string } = func(tree);
+// BEGIN (write your solution here)
+const map = (f: Node => any, node: Node) => {
+  const updatedNode = f(node);
 
-  if (!children) {
-    return { ...tree, name: updatedName };
-  }
-  return {
-    ...tree,
-    name: updatedName,
-    children: children.map(c => map(func, c)),
-  };
+  return node.type === 'directory'
+    ? { ...updatedNode, children: (node.children || []).map(n => map(f, n)) }
+    : updatedNode;
 };
 
 export default map;
 // END
-
