675b8feb9351cd770566ffb429980c7ba57592cb
Andrey Karamnoff <karamnoff@gmail.com>
2018-09-23 21:19:13 +0300

    add hexlet' solution for reduce

--- src/reduce.js
+++ src/reduce.js
@@ -1,4 +1,27 @@
 // @flow
-// BEGIN (write your solution here)
+type BaseNode = { name: string, meta: {} };
+type File = BaseNode & {
+  type: 'file'
+};
 
+/* eslint-disable no-use-before-define */
+type Directory = BaseNode & {
+  type: 'directory',
+  children: Array<Node>
+};
+/* eslint-enable no-use-before-define */
+
+type Node = File | Directory;
+
+// BEGIN (write your solution here)
+const reduce = <T>(f: (T, Node) => T, node: Node, acc: T): T => {
+  const newAcc = f(acc, node);
+
+  if (node.type === 'file') {
+    return newAcc;
+  }
+  return (node.children || []).reduce((iAcc, n) => reduce(f, n, iAcc), newAcc);
+};
+
+export default reduce;
 // END
