248c8e3cc3bb4fa8d6c1ee67f5f150dfdb1fb07b
Andrey Karamnoff <karamnoff@gmail.com>
2018-09-23 21:16:17 +0300

    add hexlet' solution for filter

--- src/filter.js
+++ src/filter.js
@@ -1,4 +1,31 @@
 // @flow
-// BEGIN (write your solution here)
+type BaseNode = { name: string, meta: {} };
+type File = BaseNode & {
+  type: 'file'
+};
 
+/* eslint-disable no-use-before-define */
+type Directory = BaseNode & {
+  type: 'directory',
+  children: Array<Node>
+};
+/* eslint-enable no-use-before-define */
+
+type Node = File | Directory;
+
+// BEGIN (write your solution here)
+const filter = (f: Node => boolean, node: Node) => {
+  if (!f(node)) {
+    return null;
+  }
+
+  return node.type === 'directory'
+    ? {
+        ...node,
+        children: (node.children || []).map(n => filter(f, n)).filter(v => v),
+      }
+    : node;
+};
+
+export default filter;
 // END
