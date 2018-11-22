3ee1ca22c15aa160063e55dbcc1fd67671562f97
Andrey Karamnoff <karamnoff@gmail.com>
2018-11-22 15:00:22 +0300

    add solution for flatten-challenge

--- src/flatten.js
+++ src/flatten.js
@@ -1,4 +1,12 @@
 // @flow
 // BEGIN (write your solution here)
+const extract = (element: mixed, accumulator: Array<mixed>): Array<mixed> => {
+  if (!Array.isArray(element)) {
+    return [...accumulator, element];
+  }
+  return element.reduce((iAcc: Array<mixed>, child: mixed) => extract(child, iAcc), accumulator);
+};
 
+export default (items: Array<mixed>): Array<mixed> =>
+  items.reduce((acc: Array<mixed>, item: mixed) => extract(item, acc), []);
 // END
