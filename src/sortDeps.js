3bd9d52f2d85118e8b2c5142cea0d3d5c9f229ae
Andrey Karamnoff <karamnoff@gmail.com>
2019-01-05 23:12:16 +0300

    add hexlet' solution for dependencies-challenge

--- src/sortDeps.js
+++ src/sortDeps.js
@@ -1,27 +1,12 @@
 // @flow
 // BEGIN (write your solution here)
-export default (obj: {[string]: Array<?string>}): Array<?string> => {
-  const topologicalSortHelper = (node: string, explored: Set<?string>, s: Array<?string>) => {
-    explored.add(node);
-    if (obj[node]) {
-      obj[node].forEach((n) => {
-        if (!explored.has(n)) {
-          topologicalSortHelper(n, explored, s);
-        }
-      });
-    }
-
-    s.push(node);
+export default (deps) => {
+  const add = (acc, node) => {
+    const subDeps = deps[node] || [];
+    const subAcc = subDeps.reduce(add, []);
+    return { ...acc, ...subAcc, [node]: true };
   };
-
-  const stack: Array<?string> = [];
-  const explored: Set<?string> = new Set();
-
-  Object.keys(obj).forEach((node) => {
-    if (!explored.has(node)) {
-      topologicalSortHelper(node, explored, stack);
-    }
-  });
-  return stack;
+  const set = Object.keys(deps).reduce(add, {});
+  return Object.keys(set);
 };
 // END
