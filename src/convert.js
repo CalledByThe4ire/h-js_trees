097f11335714c60fef75a48e68bbce4e8a3b6c36
Andrey Karamnoff <karamnoff@gmail.com>
2019-01-05 23:49:56 +0300

    add hexlet' solution for convertor-challenge

--- src/convert.js
+++ src/convert.js
@@ -1,28 +1,10 @@
-// @flow
-type Entry = [string, any];
-type Tree = (?Entry)[];
-type Map = <T>(acc: T, entry: Entry) => T;
-// BEGIN (write your solution here)
-const reduce = <T>(fn: Map, node: Entry, acc: T): T => {
-  const [key, value] = node;
-
-  if (!Array.isArray(value) || (Array.isArray(value) && value.length === 0)) {
-    return fn(acc, node);
-  }
-  return {
-    ...acc,
-    [key]: value.reduce((iAcc, n) => reduce(fn, n, iAcc), {}),
-  };
-};
-
-const map: Map = (acc, [key, value]) => ({
-  ...acc,
-  [key]: !Array.isArray(value) ? value : {},
-});
-
-export default (tree: Tree) =>
-  tree.reduce(
-    (accumulator, entry: Entry) => reduce(map, entry, accumulator),
+const convert = items =>
+  items.reduce(
+    (acc, [key, value]) => ({
+      ...acc,
+      [key]: value instanceof Array ? convert(value) : value,
+    }),
     {},
   );
-// END
+
+export default convert;
